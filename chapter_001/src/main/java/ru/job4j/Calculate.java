df160106cac07f5c3d7b440e3326085d49b7cfb3
Andrey Kov <drones2024@mail.ru>
2018-05-18 16:07:07 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -5,11 +5,20 @@ package ru.job4j.calculate;
 * @author Gilyazev Ramin
 */
 public class Calculate {
-	/**
-	* main
-	* @param args - args.
-	*/
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+    /**
+     * main
+     * @param args - args.
+     */
+     public static void main(String[] args) {
+         System.out.println("Hello World");
+     }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+     public String echo(String name) {
+         return "Echo, echo, echo : " + name;
+     }
 }
